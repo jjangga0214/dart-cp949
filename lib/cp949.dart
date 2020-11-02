5f644e6c1df410ec884b65abddf77d3ee80b0d67
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-11-03 07:12:16 +0900

    feat: impl toUtf8Code() and _loadCodeMap()

--- lib/cp949.dart
+++ lib/cp949.dart
@@ -1,3 +1,16 @@
-const message = "hello, world";
 
-void greet() => print(message);
+import 'dart:async' show Future;
+import 'dart:core' show Map, String;
+import 'dart:convert';
+import 'dart:io';
+
+
+Future<Map<String, String>> _loadCodeMap() async {
+    return json.decode(await (new File('lib/cp949-to-unicode.json')).readAsString()).cast<String, String>();
+}
+
+var codeMap = _loadCodeMap();
+
+Future<String> toUtf8Code(String cp949Code) async {
+    return (await codeMap)[cp949Code];
+}
--- test/cp949_test.dart
+++ test/cp949_test.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:test/test.dart';
 import 'package:cp949/cp949.dart' as cp949;
 
-void main(){
-    cp949.greet();
+void main() {
+  test('cp949.toUtf8Code() converts cp949 code unit to unicode unit.', () async {
+    expect(await cp949.toUtf8Code('0x8141'), equals('0xAC02'));
+  });
 }
